f26a17f891f4aef5b13de24f2bb878ad510f675f
Alistair Stead <alistair.stead@designdisclosure.com>
2011-03-16 22:47:56 +0000

    Added module code Refactored the name of the module and added config to rewrite the email_template class

--- src/app/code/community/Ibuildings/MageTest/Model/Email/Template.php
+++ src/app/code/community/Ibuildings/MageTest/Model/Email/Template.php
@@ -1,62 +1,13 @@
 <?php
 /**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Core
- * @copyright   Copyright (c) 2010 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * 
  */
 
 /**
- * Template model
- *
- * Example:
- *
- * // Loading of template
- * $emailTemplate  = Mage::getModel('core/email_template')
- *    ->load(Mage::getStoreConfig('path_to_email_template_id_config'));
- * $variables = array(
- *    'someObject' => Mage::getSingleton('some_model')
- *    'someString' => 'Some string value'
- * );
- * $emailTemplate->send('some@domain.com', 'Name Of User', $variables);
- *
- * @category   Mage
- * @package    Mage_Core
- * @author      Magento Core Team <core@magentocommerce.com>
+ * 
  */
-class Ibuildings_Test_Model_Email_Template extends Mage_Core_Model_Email_Template
+class Ibuildings_MageTest_Model_Email_Template extends Mage_Core_Model_Email_Template
 {
-    /**
-     * Retrieve mail object instance
-     *
-     * @return Zend_Mail
-     */
-    public function getMail()
-    {
-        if (is_null($this->_mail)) {
-            $this->_mail = new Zend_Mail('utf-8');
-        }
-        return $this->_mail;
-    }
-
     /**
      * Send mail to recipient
      *
@@ -122,10 +73,11 @@ class Ibuildings_Test_Model_Email_Template extends Mage_Core_Model_Email_Templat
         $mail->setFrom($this->getSenderEmail(), $this->getSenderName());
 
         try {
-            // Store the constructed mail in the application for testing
-            Mage::app()->setResponseEmail($mail);
-            // Cancel the send for testing
-            // $mail->send();
+            if (isset($_SERVER['MAGE_TEST'])) {
+                // Store the constructed mail in the application for testing
+                Mage::app()->setResponseEmail($mail);
+            }
+            $mail->send();
             $this->_mail = null;
         }
         catch (Exception $e) {
