edf00388b22f092eee7cd8b7741d6c2078d79ef4
Alistair Stead <alistair.stead@designdisclosure.com>
2011-11-01 21:57:01 +0000

    Added additional environment variables to the bootstrap

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -2,10 +2,15 @@
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../app'));
-    
-$_SERVER['MAGE_TEST'] = true;
 
 require_once APPLICATION_PATH.'/Mage.php';
+// Update setting so for correct behaviour under test
+$_SERVER['SCRIPT_NAME'] = dirname(__FILE__) . DS . 'index.php';
+$_SERVER['SCRIPT_FILENAME'] = dirname(__FILE__) . DS . 'index.php';
+// Global scope param to identfy that the application is under test
+$_SERVER['MAGE_TEST'] = true;
+// Standard Magento configuration
+$_SERVER['MAGE_IS_DEVELOPER_MODE'] = true;
 
 // Flush the cache once on execusion rather than on every test
 Mage::app()->getCacheInstance()->flush();
