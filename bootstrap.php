a3aa2f8bf5f0bdc18a1b75f7c2209db9c4fa8214
Alistair Stead <alistair.stead@designdisclosure.com>
2011-11-02 00:03:29 +0000

    removed the include for a file that is no longer used

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 // Include class dependencies that can not loaded by the autoloader
 // when running tests outside a Magento project
-// Include the patched app to ensure it is not autoloaded when we bootstrap Magento
-require_once 'src/app/code/community/Mage/core/Model/App.php';
 // Include Magento from the location on your machine
 // TODO Change to include Mage.php adding it to the include path making to project portable
-require_once '/mnt/Sites/magento.development.local/public/app/Mage.php';  
+require_once '/mnt/Sites/magento.development.local/public/app/Mage.php';
+
+require_once 'src/app/code/community/Ibuildings/Magetest/Model/Bootstrap.php'; 
 
 require_once 'src/app/code/community/Mage/Admin/Model/Session.php';
 require_once 'src/app/code/community/Mage/Core/Controller/Varien/Front.php';
